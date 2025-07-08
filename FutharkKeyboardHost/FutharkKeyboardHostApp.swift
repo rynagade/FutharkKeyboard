b0e4e749cba95ad0038708a84fd6a7b10c9e541e
Anthony <jarius327@gmail.com>
2025-07-07 22:23:35 -0500

    Initial Commit

---
+++ FutharkKeyboardHost/FutharkKeyboardHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutharkKeyboardHostApp.swift
+//  FutharkKeyboardHost
+//
+//  Created by Anthony Dallmier on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FutharkKeyboardHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
